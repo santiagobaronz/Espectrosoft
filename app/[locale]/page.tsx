4fa1e23d67c95056bc6268c17a40d7820457b425
Santiago Baron <santiagobazul@hotmail.com>
2023-08-23 23:32:42 -0500

    [Design] New component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import Main from '../../src/components/Home/Main'
 import Introduction from '../../src/components/Home/Introduction'
 import AboutUs from '../../src/components/Home/AboutUs'
+import Services from '../../src/components/Home/Services'
 
 export default async function HomePage() {
 
@@ -12,6 +13,7 @@ export default async function HomePage() {
 			<Main></Main>
 			<Introduction></Introduction>
 			<AboutUs></AboutUs>
+			<Services></Services>
 		</div>
 	)
 }
---
+++ src/components/Home/Services.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Services() {
+	return (
+		<div className=' bg-blue-section container mt-20 rounded-2xl'>
+			<div>
+				<p className='text-sm mb-5 text-gray-200 font-medium'>OUR SERVICES</p>
+				<h3 className='text- text-4xl font-bold'>We offer services that enhance your brand or business in the world of technology.</h3>
+			</div>
+
+		</div>
+	)
+}
