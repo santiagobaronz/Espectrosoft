9e088216ded3a37047dcd5618e6c8d015305ad54
Santiago Baron <santiagobazul@hotmail.com>
2023-08-24 23:13:32 -0500

    [Design] TANG

--- src/components/Home/Services.tsx
+++ src/components/Home/Services.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Services() {
 	return (
-		<div className=' bg-blue-section container mt-20 rounded-2xl'>
+		<div className=' bg-blue-section container mt-20 rounded-2xl p-8'>
 			<div>
 				<p className='text-sm mb-5 text-gray-200 font-medium'>OUR SERVICES</p>
 				<h3 className='text- text-4xl font-bold'>We offer services that enhance your brand or business in the world of technology.</h3>
