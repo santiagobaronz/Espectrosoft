756e78c8df50465b65aaa1a8b6e772d43e0d4ca7
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-10-27 12:48:02 -0500

    [Design] New design for E-commerce page

--- app/e-commerce/page.tsx
+++ app/e-commerce/page.tsx
@@ -6,7 +6,7 @@ import { contactPhoneNumber } from '../../src/components/contact'
 export default function page() {
 	return (
 		<div className='xl:mb-20 transition-all'>
-			<div className='bg-e-commerce xl:h-[650px] py-20 max-lg:px-10 flex items-center justify-center'>
+			<div className='bg-e-commerce xl:h-[650px] py-20 max-lg:px-10 flex items-center justify-center ecommerce-decoration'>
 				<div className='container lg:flex gap-x-20 items-center'>
 					<div className='lg:w-1/2'>
 						<div className='bg-[#12824E] rounded w-min'>
@@ -14,6 +14,8 @@ export default function page() {
 						</div>
 
 						<h1 className='md:text-5xl text-4xl text-[#071A2B] font-bold leading-tight my-8'>Publica tu tienda en linea y expande el crecimiento de tu marca</h1>
+
+						<p className='text-lg mb-10'>En Espectrosoft, convertimos tus sueños empresariales en realidad digital. Es el momento de llevar tu negocio al mundo en línea y expandir el crecimiento de tu marca.</p>
 
 						<div className='md:w-min'>
 							<Link href={contactPhoneNumber} target='_blank' className='w-min'>
@@ -29,9 +31,12 @@ export default function page() {
 				</div>
 			</div>
 
-			<svg xmlns="http://www.w3.org/2000/svg" className='relative z-0 max-xl:hidden' width="100%" height="152" fill="none" preserveAspectRatio="none" viewBox="0 0 1440 152">
+			<svg xmlns="http://www.w3.org/2000/svg" className='relative z-0 max-xl:hidden' width="100%" height="100" fill="none" preserveAspectRatio="none" viewBox="0 0 1440 152">
 				<path fill="#BBFABB" d="M126.597 138.74a165.412 165.412 0 0 1-67.38-52.76L0 0h1440s-20.02 25.84-59.85 32.958L211.382 150.811a165.765 165.765 0 0 1-84.785-12.071Z" />
 			</svg>
+
+
+			
 		</div>
 	)
 }
